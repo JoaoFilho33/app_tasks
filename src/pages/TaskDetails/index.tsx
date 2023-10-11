8d1e77bae3bbca3eba729e74f26f55ad59ea5a47
JoaoFilho33 <joaod.almeidafi@gmail.com>
2023-10-11 17:01:51 -0300

    Abordagem com components

---
+++ src/pages/TaskDetails/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Task } from "../TasksPage/components/..";
+
+interface TaskDetailsProps {
+  task: Task;
+}
+
+export const TaskDetails: React.FC<TaskDetailsProps> = ({ task }) => {
+  const formattedDate = task.created_at.toLocaleDateString();
+
+  return (
+    <div>
+      <p>Details:</p>
+      <p>Task ID: {task.id}</p>
+      <p>Created: {task.name}</p>
+      <p>Description: {task.description}</p>
+      <p>Created At: {formattedDate}</p>
+    </div>
+  );
+};
